9069abdfb6271ad3a061428a55e4e81d87ac79ee
erikemg <erigor0503@gmail.com>
2023-11-21 14:17:21 +0100

    first commit

---
+++ src/Materials.js
@@ -0,0 +1,84 @@
+const materials = {
+    water: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 236.5882365,
+        pounds: 453.59237,
+        milliliters: 1,
+        liters: 1000,
+        fluidOunce: 28.3495231,
+        ounce: 28.3495231
+    },
+    flour: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 125,
+        pounds: 453.59237,
+        milliliters: 0.528,
+        liters: 528,
+        fluidOunce: 14.9685481968,
+        ounce: 28.3495231
+    },
+    sugar: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 200.3480922,
+        pounds: 453.59237,
+        milliliters: 0.85211336848478,
+        liters: 852.11336848478,
+        fluidOunce: 24.15700762367808,
+        ounce: 28.3495231
+    },
+    oliveOil: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 212.92941285,
+        pounds: 453.59237,
+        milliliters: 0.9,
+        liters: 900,
+        fluidOunce: 25.51457079,
+        ounce: 28.3495231
+    },
+    eggWhites: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 223.2,
+        pounds: 453.59237,
+        milliliters: 0.93,
+        liters: 930,
+        fluidOunce: 26.365056483,
+        ounce: 28.3495231
+    },
+    oats: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 89.90352987,
+        pounds: 453.59237,
+        milliliters: 0.38,
+        liters: 380,
+        fluidOunce: 10.772818778,
+        ounce: 28.3495231
+    },
+    cocoa: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 85,
+        pounds: 453.59237,
+        milliliters: 0.36,
+        liters: 360,
+        fluidOunce: 10.205828316,
+        ounce: 28.3495231
+    },
+    greekYoghurt: {
+        grams: 1,
+        kilograms: 1000,
+        cups: 243.68588411,
+        pounds: 453.59237,
+        milliliters: 1.03,
+        liters: 1030,
+        fluidOunce: 29.200008793,
+        ounce: 28.3495231
+    }
+}
+
+export default materials
